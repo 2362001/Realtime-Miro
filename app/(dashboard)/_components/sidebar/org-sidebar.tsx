be93c062fc3f5ad1529cd9ab48cf5a7c1aa779e7
2362001 <o0othanh2k1@gmail.com>
2024-03-02 23:40:29 +0700

    upadte ui

--- app/(dashboard)/_components/sidebar/org-sidebar.tsx
+++ app/(dashboard)/_components/sidebar/org-sidebar.tsx
@@ -2,9 +2,7 @@ import React from "react";
 
 const OrgSidebar = () => {
   return (
-    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 bg-red-500 ">
-      OrgSidebar
-    </div>
+    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 bg-red-500 "></div>
   );
 };
 
