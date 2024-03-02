a5246149c4a3ce30edffd32becb2d7919e4c6aea
2362001 <o0othanh2k1@gmail.com>
2024-03-02 23:51:38 +0700

    update 2/3/2024 11:51 pm

--- app/(dashboard)/_components/sidebar/item.tsx
+++ app/(dashboard)/_components/sidebar/item.tsx
@@ -4,6 +4,7 @@ import { useOrganization, useOrganizationList } from "@clerk/nextjs";
 import Image from "next/image";
 
 import { cn } from "@/lib/utils";
+import { Hint } from "@/components/hint";
 
 interface ItemProps {
   id: string;
@@ -25,18 +26,18 @@ export const Item = ({ id, name, imageUrl }: ItemProps) => {
 
   return (
     <div className="aspect-square relative">
-      {/* <Hint label={name} side="right" align="start" sideOffset={18}> */}
-      <Image
-        fill
-        alt={name}
-        src={imageUrl}
-        onClick={onClick}
-        className={cn(
-          "rounded-md cursor-pointer opacity-75 hover:opacity-100 transition",
-          isActive && "opacity-100"
-        )}
-      />
-      {/* </Hint> */}
+      <Hint label={name} side="right" align="start" sideOffset={18}>
+        <Image
+          fill
+          alt={name}
+          src={imageUrl}
+          onClick={onClick}
+          className={cn(
+            "rounded-md cursor-pointer opacity-75 hover:opacity-100 transition",
+            isActive && "opacity-100"
+          )}
+        />
+      </Hint>
     </div>
   );
 };
