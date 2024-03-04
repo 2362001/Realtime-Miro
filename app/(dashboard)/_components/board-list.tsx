27360202f3efa45d0c87db1c71ee572aa1a3f382
Sena.Nguyen <Sena.Nguyen@avepoint.com>
2024-03-05 00:52:24 +0700

    update 12h52 pm 5/3/2024

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -1,13 +1,37 @@
-"use client"
+"use client";
 
-import React from 'react'
+import { useQuery } from "convex/react";
+import React from "react";
+import { api } from "@/convex/_generated/api";
+import { EmptySearch } from "./empty-search";
+import { EmptyFavorites } from "./empty-favorites";
 
-const BoardList = () => {
-  return (
-    <div>
-        Board List
-    </div>
-  )
+interface BoardListProps {
+  orgId: string;
+  query: {
+    search?: string;
+    favorites?: string;
+  };
 }
+const BoardList = ({ orgId, query }: BoardListProps) => {
+  // const data = useQuery(api.boards.get, {
+  //   orgId,
+  //   ...query,
+  // });
+  const data = []
+  if (!data?.length && query.search) {
+    return <EmptySearch />;
+  }
 
-export default BoardList ; 
+  if (!data?.length && query.favorites) {
+    return <EmptyFavorites/>;
+  }
+
+  // if (!data?.length) {
+  //   return <EmptyBoards />;
+  // }
+
+  return <div>Board List</div>;
+};
+
+export default BoardList;
